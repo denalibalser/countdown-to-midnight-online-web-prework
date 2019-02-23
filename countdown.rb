45715af02da71f36befe384d63d284b40bdcce9a
Denali Balser <denali.balser167@gmail.com>
2019-02-23 19:51:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,10 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1 sleep(5.seconds)
+  end
+  return "HAPPY NEW YEAR!"
 end 
